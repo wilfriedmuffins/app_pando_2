8e04370ef0ddd3d2a82263a3c581e7cb14d44c76
wilfriedmuffins <wilfried.moffen@live.fr>
2023-02-20 02:17:58 +0100

    filter done

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -1,18 +1,10 @@
 class MeasuresController < ApplicationController
     def index
-            @query = Measure.ransack(params[:q])
-            @measures = @query.result(distinct: true)
-         
-            @measures_co2 = @measures.filter_by_measure_type("CO2")
-            @measures_tmp = @measures.filter_by_measure_type("TMP")
-            @measures_voct = @measures.filter_by_measure_type("VOCT")
-            @measures_hum = @measures.filter_by_measure_type("HUM")
-    
-            # @measures = Measure.all
-            # @measures_co2 = @measures.filter_by_measure_type("CO2")
-            # @measures_tmp = @measures.filter_by_measure_type("TMP")
-            # @measures_voct = @measures.filter_by_measure_type("VOCT")
-            # @measures_hum = @measures.filter_by_measure_type("HUM")
-
+        @query = Measure.ransack(params[:q])
+        @measures = @query.result(distinct: true)
+        @measures_co2 = @measures.filter_by_measure_type("CO2")
+        @measures_tmp = @measures.filter_by_measure_type("TMP")
+        @measures_voct = @measures.filter_by_measure_type("VOCT")
+        @measures_hum = @measures.filter_by_measure_type("HUM")
     end
 end
