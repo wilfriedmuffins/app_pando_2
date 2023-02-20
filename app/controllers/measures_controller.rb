c27ade4266e6794dbda583a400d30590cedf55bf
wilfriedmuffins <wilfried.moffen@live.fr>
2023-02-20 01:10:43 +0100

    filter done

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -1,22 +1,18 @@
 class MeasuresController < ApplicationController
     def index
+            @query = Measure.ransack(params[:q])
+            @measures = @query.result(distinct: true)
+         
+            @measures_co2 = @measures.filter_by_measure_type("CO2")
+            @measures_tmp = @measures.filter_by_measure_type("TMP")
+            @measures_voct = @measures.filter_by_measure_type("VOCT")
+            @measures_hum = @measures.filter_by_measure_type("HUM")
+    
+            # @measures = Measure.all
+            # @measures_co2 = @measures.filter_by_measure_type("CO2")
+            # @measures_tmp = @measures.filter_by_measure_type("TMP")
+            # @measures_voct = @measures.filter_by_measure_type("VOCT")
+            # @measures_hum = @measures.filter_by_measure_type("HUM")
 
-        @query = Measure.ransack(params[:q])
-        @measures = @query.result(distinct: true)
-        #@measures = Measure.all
-        @measures_co2 = @measures.filter_by_measure_type("CO2")
-        @measures_tmp = @measures.filter_by_measure_type("TMP")
-        @measures_voct = @measures.filter_by_measure_type("VOCT")
-        @measures_hum = @measures.filter_by_measure_type("HUM")
-        # if params[:query].present?
-            
-        #   else
-        #     @measures = Measure.all
-        #     @measures_co2 = @measures.filter_by_measure_type("CO2")
-        #     @measures_tmp = @measures.filter_by_measure_type("TMP")
-        #     @measures_voct = @measures.filter_by_measure_type("VOCT")
-        #     @measures_hum = @measures.filter_by_measure_type("HUM")
-        #   end
-        
     end
 end
